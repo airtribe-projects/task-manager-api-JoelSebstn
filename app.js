28d8ce49ba7328f657345756f3eeacfc38e0bb07
Joel Sebastian✨ <joel.sebastian@experionglobal.com>
2026-03-24 23:48:20 +0530

    Revert "assignment #1"
    This reverts commit bfb41bf70a0911fa6d473f4a4e75253750098e81.

--- app.js
+++ app.js
@@ -1,20 +1,16 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-const router = require("./v1/tasks/tasks.router");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use("/", router);
 
-if (require.main === module) {
-    app.listen(port, (err) => {
-        if (err) {
-            return console.log('Something bad happened', err);
-        }
-        console.log(`Server is listening on ${port}`);
-    });
-}
+app.listen(port, (err) => {
+    if (err) {
+        return console.log('Something bad happened', err);
+    }
+    console.log(`Server is listening on ${port}`);
+});
 
 
 
